893ac3880f8f99967c59c3ec184655ac5d93dd68
Mstbr23 <omar.moustabchir@outlook.com>
2023-02-16 14:33:28 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A c small program that prints exactly a given line  
- * return (1),succss
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ * followed by a new line, to standard error.
+ * return: Always 1.
  */
 int main(void)
 {
-fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return 1;
+write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+return (1);
 }
