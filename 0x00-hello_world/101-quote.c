aea8117cbe451af3ddaa45b202597a77ad3b094d
Mstbr23 <omar.moustabchir@outlook.com>
2023-02-16 14:05:46 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A c small program 
+ * return (1)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
