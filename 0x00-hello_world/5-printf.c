11822b3acc880b989fd2466cec9d8c1b192ece03
Mstbr23 <omar.moustabchir@outlook.com>
2023-02-16 12:48:44 -0800

    5-printf.c update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-/** main - main block
- * return: o
+/*
+ * main - main bloc
+ * return: 0
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+	return(0);
 }
+
