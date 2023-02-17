d80bc4cc22c6106e49e5424313385b2d436f08e4
Mstbr23 <omar.moustabchir@outlook.com>
2023-02-17 11:47:51 -0800

    0-positive_or_negative.c task

---
+++ 0-positive_or_negative.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+  * Main - main entry point
+  * Description: ' assign a random number to the variable n each time it is executed'
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%i is positive\n", n);
+	else if (n < 0)
+		printf("%i is negative\n", n);
+	else
+		printf("%i is zeor\n", n);
+	return (0);
+}
