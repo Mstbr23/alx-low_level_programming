6578c236abad1a5bb92d3a5694b76e9f30c35896
Mstbr23 <omar.moustabchir@outlook.com>
2023-02-17 03:28:01 -0800

    5-printf.c update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-/*
- * main - main bloc
+/**
+ * main - main entry point
+ * Description: 'a C program that prints using only printf'
  * return: 0
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
 
