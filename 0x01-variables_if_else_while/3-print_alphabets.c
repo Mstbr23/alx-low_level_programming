477ed7e11ce3a36dee1ee33ac85ee23f35ce418d
Mstbr23 <omar.moustabchir@outlook.com>
2023-02-17 13:22:31 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  * Description: 'a C program that prints in lowercase & then in uppercase'
+  * Return: Always  0 (Success)
+  */
+int main(void)
+{
+	char cc;
+
+	char dd;
+
+	cc = 'a';
+	dd = 'A';
+
+	while
+		(cc <= 'z')
+		{
+			putchar(cc);
+			cc++;
+		}
+	while
+		(dd <= 'z')
+		{
+			putchar(dd);
+			dd++;
+		}
+	return (0);
+}
