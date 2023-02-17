39f564850a2bf7405a8416f3a5960f4bcaaf8a73
Mstbr23 <omar.moustabchir@outlook.com>
2023-02-17 14:54:42 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  * Description: 'a C program that print a specific text'
+  * Return: Always  0 (Success)
+  */
+int main(void)
+{
+	int aa;
+
+	for (aa = 0; aa < 100; aa++)
+	{
+		putchar(aa + '0');
+		if (aa < 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
