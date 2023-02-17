6182d2f143b51b6276055b445c7473642fef15de
Mstbr23 <omar.moustabchir@outlook.com>
2023-02-17 13:45:28 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  * Description: 'a C program that print a specific text'
+  * Return: Always  0 (Success)
+  */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 100; a++)
+	{
+		putchar(a);
+		putchar('\n');
+	}
+	return (0);
+}
