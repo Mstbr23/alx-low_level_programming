d6b10e1d3ebea505f2b605c953aaf0697024b6d7
Mstbr23 <omar.moustabchir@outlook.com>
2023-02-17 14:36:40 -0800

    7-print_tebahpla.c task

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  * Description: 'a C program that print a specific text'
+  * Return: Always  0 (Success)
+  */
+int main(void)
+{
+	char cc = 'z';
+
+	while
+		(cc >= 'a')
+		{
+			putchar(cc);
+			cc--;
+		}
+	putchar('\n');
+	return (0);
+}
