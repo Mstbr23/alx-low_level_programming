62787a87dca1792b3a04561d530a1399d96c30ef
Mstbr23 <omar.moustabchir@outlook.com>
2023-02-18 06:14:17 -0800

    101-print_comb4.c update

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,33 +6,16 @@
   */
 int main(void)
 {
-int i, j, k;
-for (i = 0; i < 10; i++)
+for (int i = 0; i <= 7; i++)
 {
-for (j = i + 1; j < 10; j++)
+for (int j = i + 1; j <= 8; j++)
 {
-for (k = j + 1; k < 10; k++)
-{
-if (i != j && i != k && j != k)
-{
-int smallest = (i < j && i < k) ? i : ((j < k) ? j : k);
-putchar(smallest + '0');
-if (smallest == i)
-{
-putchar(j + '0');
-putchar(k + '0');
-}
-else if (smallest == j)
-{
-putchar(i + '0');
-putchar(k + '0');
-}
-else
+for (int k = j + 1; k <= 9; k++)
 {
 putchar(i + '0');
 putchar(j + '0');
-}
-if (i < 7 || (i == 7 && j < 8))
+putchar(k + '0');
+if (i != 7 || j != 8 || k != 9)
 {
 putchar(',');
 putchar(' ');
@@ -40,7 +23,5 @@ putchar(' ');
 }
 }
 }
-}
-putchar('\n');
 return (0);
 }
