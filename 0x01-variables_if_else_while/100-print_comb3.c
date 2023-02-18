0114375b4497c03290b535edf27ec52d99af8f0d
Mstbr23 <omar.moustabchir@outlook.com>
2023-02-18 04:19:24 -0800

    gcc 9-print_comb.c -o 9-print_comb

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,12 +12,15 @@ for (i = 0; i < 10; i++)
 {
 for (j = i + 1; j < 10; j++)
 {
-if (i != j)
+if (i != j )
 {
 putchar(i + '0');
 putchar(j + '0');
+if (i < 8 || j < 9)
+{
 putchar(',');
 putchar(' ');
+}
 }
 }
 }
