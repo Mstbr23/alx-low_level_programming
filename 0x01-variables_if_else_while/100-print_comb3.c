da6599e8118957e69409d41cdbb61ce46f19e7a7
Mstbr23 <omar.moustabchir@outlook.com>
2023-02-18 03:35:34 -0800

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * main - Entry point
+  * Description: 'a C program that print a specific text'
+  * Return: Always  0 (Success)
+  */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			putchar(' ');
+		}
+	}
+	return (0);
+}
