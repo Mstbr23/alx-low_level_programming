f40849fb683f5a5a35bae9e388254c2aef67e076
Mstbr23 <omar.moustabchir@outlook.com>
2023-02-18 03:59:12 -0800

    100-print_comb3.c update check

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,18 +7,20 @@
   */
 int main(void)
 {
-	int i;
-	int j;
-
-	for (i = 0; i <= 9; i++)
-	{
-		for (j = 0; j <= 9; j++)
-		{
-			putchar(i + '0');
-			putchar(j + '0');
-			putchar(',');
-			putchar(' ');
-		}
-	}
-	return (0);
+int i, j;
+for (i = 0; i < 10; i++)
+{
+for (j = i + 1; j < 10; j++)
+{
+if (i != j)
+{
+putchar(i + '0');
+putchar(j + '0');
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
 }
