b97f0e4714e788d46e785067fa15b27c4eeba6c3
Mstbr23 <omar.moustabchir@outlook.com>
2023-02-23 07:44:40 -0800

    4. I believe in numbers and signs

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* print_most_numbers - 4. I believe in numbers and signs.
+*
+* Return: Always 0.
+*/
+void print_most_numbers(void)
+{
+int c;
+for (c = 48; c < 58; c++)
+{
+if (c != 50)
+{
+if (c != 52)
+{
+_putchar(c);
+}
+}
+}
+_putchar('\n');
+}
