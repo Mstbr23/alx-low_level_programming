74b304490db7beb5a006cce880e2063108904b11
Mstbr23 <omar.moustabchir@outlook.com>
2023-03-06 06:03:11 -0800

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _strchr - Entry point
+* @s: input to use.
+* @c: input to use.
+* Return: Always 0 (Success)
+*/
+char *_strchr(char *ss, char c)
+{
+int i = 0;
+
+for (; ss[i] >= '\0'; i++)
+{
+if (ss[i] == c)
+return (&ss[i]);
+}
+return (0);
+}
