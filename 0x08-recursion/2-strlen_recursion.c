9699a93161337bf7cecb4032ba93f97f91a68674
Mstbr23 <omar.moustabchir@outlook.com>
2023-03-08 09:12:13 -0800

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _strlen_recursion - Returns the length of a string.
+* @str: The string to be measured.
+* Return: The length of the string.
+*/
+int _strlen_recursion(char *str)
+{
+int lng = 0;
+
+if (*str)
+{
+lng++;
+lng += _strlen_recursion(str + 1);
+}
+return (lng);
+}
